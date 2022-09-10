65304a296cd4e2ad97656e652d65a42714c0e347
HodaGamal <77369927+Hoda233@users.noreply.github.com>
2022-09-11 00:09:48 +0200

    Add files via upload

---
+++ Section 8: Component Communication/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <form @submit.prevent="submitData">
+      <div>
+        <label>Name </label>
+        <input type="text" v-model="inputName" />
+      </div>
+      <div>
+        <label>Age </label>
+        <input type="text" v-model="inputAge" />
+      </div>
+      <button>Set User Data</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  emits: ["set-user"],
+  data() {
+    return {
+      inputName: "",
+      inputAge: "",
+    };
+  },
+
+  methods: {
+    submitData() {
+      this.$emit("set-user", this.inputName, this.inputAge);
+    },
+  },
+};
+</script>
