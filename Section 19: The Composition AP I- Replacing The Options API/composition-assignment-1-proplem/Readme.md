4d5e150fa425a25e9884f4b5fe635ca3985911e8
HodaGamal <77369927+Hoda233@users.noreply.github.com>
2022-09-30 16:46:14 +0200

    Add files via upload

---
+++ Section 19: The Composition AP I- Replacing The Options API/composition-assignment-1-proplem/Readme.md
@@ -0,0 +1,2 @@
+![text](Screenshot%20(2065).png)
+![text](Screenshot%20(2066).png)
---
+++ Section 19: The Composition AP I- Replacing The Options API/composition-assignment-1-proplem/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <h2>My Course Goal</h2>
+  <!-- Task 1: Output your main course goal with help of the composition API -->
+  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+  <h3 v-if="goalIsVisible">{{courseGoal}}</h3>
+  <h3 v-if="courseData.goalIsVisible">{{courseData.courseGoal}}</h3>
+  <h3 v-if="courseDataRec.goalIsVisible">{{courseDataRec.courseGoal}}</h3>
+  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+  <button @click="toogleGoalVisibility">Toggle Goal</button>
+  <!-- Task 3: Manage data in three ways -->
+  <!-- => Separate refs -->
+  <!-- => Ref Object -->
+  <!-- => Reactive Object -->
+  <!-- Task 4: Also solve the assignment with the Options API -->
+</template>
+
+<script>
+  import {reactive, ref} from 'vue';
+export default {
+  setup(){
+
+    const courseGoal=ref('Master Vue');
+    const goalIsVisible=ref(false);
+
+    const courseData=ref({
+      courseGoal:'Master Vue',
+      goalIsVisible:false,
+    });
+
+    const courseDataRec=reactive({
+      courseGoal:'Master Vue',
+      goalIsVisible:false,
+    });
+
+    function toogleGoalVisibility(){
+      goalIsVisible.value=!goalIsVisible.value;
+      courseData.value.goalIsVisible=!courseData.value.goalIsVisible;
+      courseDataRec.goalIsVisible=!courseDataRec.goalIsVisible;
+    }
+
+    return {courseGoal,goalIsVisible,toogleGoalVisibility,courseData,courseDataRec};
+  }
+}
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
